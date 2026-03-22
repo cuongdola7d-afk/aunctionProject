0e95c182ef8b17632af20f67d555c615091f8fab
25020124-el-hnim-cud <25020124@vnu.edu.vn>
2026-03-23 00:26:43 +0700

    changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.aunction {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.aunction to javafx.fxml;
-    exports com.aunction;
+    opens com.aunction.controller to javafx.fxml;
+    exports com.aunction.controller;
 }
