e0c7754abed50ade9444d290fb29e49a0588140d
cuongdomm123 <cuongdola7d@gmail.com>
2026-03-25 20:39:59 +0700

    feat: itemDAO + base id randomizer

---
+++ src/main/java/com/aunction/DDCAunction.java
@@ -0,0 +1,7 @@
+package com.aunction;
+
+public class DDCAunction {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- src/main/java/com/aunction/entity/item/Electronics.java
+++ src/main/java/com/aunction/entity/item/Electronics.java
@@ -4,9 +4,10 @@ public class Electronics extends Item {
     private String brand;
     private int warrantyMonths;
 
-    public Electronics(String brand, int warrantyMonths){
-          this.brand = brand;
-          this.warrantyMonths = warrantyMonths;
+    public Electronics(String id, String name, String desc, double price, String brand, int warrantyMonths) {
+        super(name, desc, price);
+        this.brand = brand;
+        this.warrantyMonths = warrantyMonths;
     }
 
     @Override
