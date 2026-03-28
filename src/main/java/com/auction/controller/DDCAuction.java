047b5727e2f179fb669dc9fed52f86b1ed3644af
cuongdomm123 <cuongdola7d@gmail.com>
2026-03-28 14:23:22 +0700

    feat: stage + scene + icon

--- src/main/java/com/auction/controller/DDCAuction.java
+++ src/main/java/com/auction/controller/DDCAuction.java
@@ -1,6 +1,12 @@
 package com.auction.controller;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;;
 
 public class DDCAuction extends Application {
     public static void main(String[] args) {
@@ -8,6 +14,15 @@ public class DDCAuction extends Application {
     }
 
     public void start(Stage stage) throws Exception{
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLUEVIOLET);
+        
+        Image icon = new Image(getClass().getResourceAsStream("/com/auction/Image/icon.png"));
+
+        stage.setTitle("DDCAuction");
+        stage.getIcons().add(icon);
+
+        stage.setScene(scene);
         stage.show();
     }
 }
