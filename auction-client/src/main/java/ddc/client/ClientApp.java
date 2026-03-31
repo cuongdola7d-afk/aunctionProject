ba31e5b2c7d3e73ad95e2cf28806ed6d5ab4952e
cuongdomm123 <cuongdola7d@gmail.com>
2026-04-01 03:21:09 +0700

    feat: Second UploadItemDialog

--- auction-client/src/main/java/ddc/client/ClientApp.java
+++ auction-client/src/main/java/ddc/client/ClientApp.java
@@ -1,25 +1,29 @@
 package ddc.client;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class ClientApp extends Application {
-
+public class ClientApp extends Application{
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(
-                getClass().getResource("/ddc/client/views/bidding/bidding.fxml")
-        );
+    public void start (Stage stage) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("views/loginregister/Login.fxml"));
+        Parent root = loader.load();
 
-        Scene scene = new Scene(loader.load(), 1180, 760);
-        scene.getStylesheets().add(
-                getClass().getResource("/ddc/client/css/bidding/bidding.css").toExternalForm()
-        );
+        Image icon = new Image(getClass().getResourceAsStream("views/DDCAuction.png"));
 
-        stage.setTitle("DDC Auction - Bidding");
+        Scene scene = new Scene(root, 400, 500);
+
+        stage.getIcons().add(icon);
+
+        stage.setTitle("DDC Auction");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
