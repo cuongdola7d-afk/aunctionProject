a2334c93eea533db877653d262d19f8b6df7081c
25020124-el-hnim-cud <25020124@vnu.edu.vn>
2026-04-01 22:56:48 +0700

    demo HOME

--- auction-client/src/main/java/ddc/client/controller/bidding/BiddingController.java
+++ auction-client/src/main/java/ddc/client/controller/bidding/BiddingController.java
@@ -1,5 +1,9 @@
 package ddc.client.controller.bidding;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import ddc.client.model.AuctionItemViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +11,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BiddingController {
 
@@ -36,19 +36,19 @@ public class BiddingController {
     }
 
     private void loadSampleData() {
-        itemList.add(new AuctionItemViewModel("Đồng hồ thông minh", "1,250,000 đ", "02:15:30", "/ddc/client/images/watch.jpg"));
-        itemList.add(new AuctionItemViewModel("Máy ảnh Vintage", "3,400,000 đ", "00:45:12", "/ddc/client/images/camera.jpg"));
-        itemList.add(new AuctionItemViewModel("Tai nghe chống ồn", "850,000 đ", "05:10:00", "/ddc/client/images/headphone.jpg"));
-        itemList.add(new AuctionItemViewModel("Bàn phím cơ RGB", "2,100,000 đ", "01:20:45", "/ddc/client/images/keyboard.jpg"));
-        itemList.add(new AuctionItemViewModel("Màn hình 4K", "6,500,000 đ", "12:05:00", "/ddc/client/images/monitor.jpg"));
-        itemList.add(new AuctionItemViewModel("Chuột Gaming Wireless", "1,150,000 đ", "00:15:00", "/ddc/client/images/mouse.jpg"));
-        itemList.add(new AuctionItemViewModel("MacBook Pro", "22,000,000 đ", "23:45:10", "/ddc/client/images/laptop.jpg"));
-        itemList.add(new AuctionItemViewModel("Loa Bluetooth", "4,200,000 đ", "08:30:00", "/ddc/client/images/speaker.jpg"));
+        itemList.add(new AuctionItemViewModel("Đồng hồ thông minh", "1,250,000 đ", "02:15:30", "/ddc/client/views/bidding/image/watch.jpg"));
+        itemList.add(new AuctionItemViewModel("Máy ảnh Vintage", "3,400,000 đ", "00:45:12", "/ddc/client/views/bidding/image/camera.jpg"));
+        itemList.add(new AuctionItemViewModel("Tai nghe chống ồn", "850,000 đ", "05:10:00", "/ddc/client/views/bidding/image/headphone.jpg"));
+        itemList.add(new AuctionItemViewModel("Bàn phím cơ RGB", "2,100,000 đ", "01:20:45", "/ddc/client/views/bidding/image/keyboard.jpg"));
+        itemList.add(new AuctionItemViewModel("Màn hình 4K", "6,500,000 đ", "12:05:00", "/ddc/client/views/bidding/image/monitor.jpg"));
+        itemList.add(new AuctionItemViewModel("Chuột Gaming Wireless", "1,150,000 đ", "00:15:00", "/ddc/client/views/bidding/image/mouse.jpg"));
+        itemList.add(new AuctionItemViewModel("MacBook Pro", "22,000,000 đ", "23:45:10", "/ddc/client/views/bidding/image/laptop.jpg"));
+        itemList.add(new AuctionItemViewModel("Loa Bluetooth", "4,200,000 đ", "08:30:00", "/ddc/client/views/bidding/image/speaker.jpg"));
 
-        itemList.add(new AuctionItemViewModel("Đồng hồ thể thao", "1,850,000 đ", "03:20:10", "/ddc/client/images/watch.jpg"));
-        itemList.add(new AuctionItemViewModel("Máy ảnh Canon", "5,400,000 đ", "00:25:00", "/ddc/client/images/camera.jpg"));
-        itemList.add(new AuctionItemViewModel("Tai nghe Gaming", "1,100,000 đ", "06:40:32", "/ddc/client/images/headphone.jpg"));
-        itemList.add(new AuctionItemViewModel("Bàn phím Bluetooth", "950,000 đ", "01:55:12", "/ddc/client/images/keyboard.jpg"));
+        itemList.add(new AuctionItemViewModel("Đồng hồ thể thao", "1,850,000 đ", "03:20:10", "/ddc/client/views/bidding/image/watch.jpg"));
+        itemList.add(new AuctionItemViewModel("Máy ảnh Canon", "5,400,000 đ", "00:25:00", "/ddc/client/views/bidding/image/camera.jpg"));
+        itemList.add(new AuctionItemViewModel("Tai nghe Gaming", "1,100,000 đ", "06:40:32", "/ddc/client/views/bidding/image/headphone.jpg"));
+        itemList.add(new AuctionItemViewModel("Bàn phím Bluetooth", "950,000 đ", "01:55:12", "/ddc/client/views/bidding/image/keyboard.jpg"));
     }
 
     private void renderItems(List<AuctionItemViewModel> items) {
