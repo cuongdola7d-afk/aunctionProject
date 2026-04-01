ca6cb5db5e68bf9a61db4d8e7a5f0380e67d294c
cuongdomm123 <cuongdola7d@gmail.com>
2026-04-02 00:39:24 +0700

    fix: Changing source for Testing

--- auction-client/src/main/java/ddc/client/controller/loginregister/Login.java
+++ auction-client/src/main/java/ddc/client/controller/loginregister/Login.java
@@ -30,7 +30,7 @@ public class Login {
         else {
             try {
                 ((Stage) ((Node) event.getSource()).getScene().getWindow()).close();
-                Parent root = FXMLLoader.load(getClass().getResource("/ddc/client/views/home/home.fxml"));
+                Parent root = FXMLLoader.load(getClass().getResource("/ddc/client/views/selling/Selling.fxml"));
                 Stage stage = new Stage();
                 Image icon = new Image(getClass().getResourceAsStream("/ddc/client/views/DDCAuction.png"));
 
