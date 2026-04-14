21e03c924e5debc9dcdcee92013e46e9e661b6ce
25020124-el-hnim-cud <25020124@vnu.edu.vn>
2026-04-14 22:35:01 +0700

    module

--- auction-server/src/main/java/module-info.java
+++ auction-server/src/main/java/module-info.java
@@ -9,7 +9,7 @@ module ddc.server {
     exports ddc.server.model.item;
     exports ddc.server.model.transaction;
     exports ddc.server.model.user;
-    exports ddc.server.network;
+    exports ddc.server.network.client;
     exports ddc.server.pattern.observer;
     exports ddc.server.service;
 
@@ -17,6 +17,6 @@ module ddc.server {
     opens ddc.server.model.item to com.google.gson;
     opens ddc.server.model.transaction to com.google.gson;
     opens ddc.server.model.user to com.google.gson;
-    opens ddc.server.network to com.google.gson;
+    opens ddc.server.network.client to com.google.gson;
     opens ddc.server.pattern.observer to com.google.gson;
 }
