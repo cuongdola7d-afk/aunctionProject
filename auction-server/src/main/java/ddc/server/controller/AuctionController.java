fd6b8b4933c3b84fed1512b6721d98fb91ce6d88
25020124-el-hnim-cud <25020124@vnu.edu.vn>
2026-04-15 22:52:08 +0700

    stone

--- auction-server/src/main/java/ddc/server/controller/AuctionController.java
+++ auction-server/src/main/java/ddc/server/controller/AuctionController.java
@@ -1,57 +1,57 @@
-// package ddc.server.controller;
+// // package ddc.server.controller;
 
-import java.util.List;
+// import java.util.List;
 
-import ddc.server.exception.AuctionClosedException;
-import ddc.server.exception.InvalidBidException;
-import ddc.server.model.transaction.Auction;
-import ddc.server.model.transaction.AuctionStatus;
-import ddc.server.model.transaction.BidTransaction;
-import ddc.server.model.user.Bidder;
-import ddc.server.service.AuctionService;
+// import ddc.server.exception.AuctionClosedException;
+// import ddc.server.exception.InvalidBidException;
+// import ddc.server.model.transaction.Auction;
+// import ddc.server.model.transaction.AuctionStatus;
+// import ddc.server.model.transaction.BidTransaction;
+// import ddc.server.model.user.Bidder;
+// import ddc.server.service.AuctionService;
 
-// public class AuctionController {
-//     private final AuctionService auctionService;
+// // public class AuctionController {
+// //     private final AuctionService auctionService;
 
-    public AuctionController() {
-        this(new AuctionService());
-    }
-
-    public AuctionController(AuctionService auctionService) {
-        this.auctionService = auctionService;
-    }
-
-//     public void handleStartAuction(Auction auction) throws AuctionClosedException, InvalidBidException {
-//         auctionService.startAuction(auction);
+//     public AuctionController() {
+//         this(new AuctionService());
 //     }
 
-//     public void handlePlaceBid(Auction auction, Bidder bidder, double amount)
-//             throws InvalidBidException, AuctionClosedException {
-//         auctionService.placeBid(auction, bidder, amount);
+//     public AuctionController(AuctionService auctionService) {
+//         this.auctionService = auctionService;
 //     }
 
-//     public void handleFinishAuction(Auction auction) throws InvalidBidException {
-//         auctionService.finishAuction(auction);
+// //     public void handleStartAuction(Auction auction) throws AuctionClosedException, InvalidBidException {
+// //         auctionService.startAuction(auction);
+// //     }
+
+// //     public void handlePlaceBid(Auction auction, Bidder bidder, double amount)
+// //             throws InvalidBidException, AuctionClosedException {
+// //         auctionService.placeBid(auction, bidder, amount);
+// //     }
+
+// //     public void handleFinishAuction(Auction auction) throws InvalidBidException {
+// //         auctionService.finishAuction(auction);
+// //     }
+
+// //     public AuctionStatus handleRefreshStatus(Auction auction) {
+// //         auctionService.refreshAuctionStatus(auction);
+// //         return auction.getStatus();
+// //     }
+
+// //     public double getCurrentPrice(Auction auction) {
+// //         return auctionService.getCurrentPrice(auction);
+// //     }
+
+// //     public Bidder getHighestBidder(Auction auction) {
+// //         return auctionService.getHighestBidder(auction);
+// //     }
+
+//     public List<BidTransaction> getBidHistory(Auction auction) {
+//         return auctionService.getBidHistory(auction);
 //     }
 
-//     public AuctionStatus handleRefreshStatus(Auction auction) {
-//         auctionService.refreshAuctionStatus(auction);
-//         return auction.getStatus();
+//     public AuctionService getAuctionService() {
+//         return auctionService;
 //     }
-
-//     public double getCurrentPrice(Auction auction) {
-//         return auctionService.getCurrentPrice(auction);
-//     }
-
-//     public Bidder getHighestBidder(Auction auction) {
-//         return auctionService.getHighestBidder(auction);
-//     }
-
-    public List<BidTransaction> getBidHistory(Auction auction) {
-        return auctionService.getBidHistory(auction);
-    }
-
-    public AuctionService getAuctionService() {
-        return auctionService;
-    }
-}
+// }
