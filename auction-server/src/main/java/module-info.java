812a85e646b93d5e5496b75be211ddceb6e6d8b3
Nguyễn Văn Đăng <duoctongbg123@gmail.com>
2026-05-10 22:56:44 +0700

    .

--- auction-server/src/main/java/module-info.java
+++ auction-server/src/main/java/module-info.java
@@ -5,25 +5,10 @@ module ddc.server {
     requires com.zaxxer.hikari;
 
     exports ddc.server;
-    // exports ddc.server.config;
-    // exports ddc.server.dao;
-    // exports ddc.server.exception;
-    // exports ddc.server.model.entity;
-    // exports ddc.server.model.item;
-    // exports ddc.server.model.transaction;
-    // exports ddc.server.model.user;
-    // exports ddc.server.network;
-    // exports ddc.server.network.client;
-    // exports ddc.server.network.message;
-    // exports ddc.server.network.request;
-    // exports ddc.server.network.response;
-    // exports ddc.server.pattern.Singleton;
-    // exports ddc.server.pattern.observer;
-    // exports ddc.server.service;
 
     opens ddc.server.controller to com.google.gson;
     opens ddc.server.controller.handler to com.google.gson;
-    opens ddc.server.controller.service to com.google.gson, org.junit.platform.commons; 
+    opens ddc.server.controller.service to com.google.gson, org.junit.platform.commons;
 
     opens ddc.server.model.entity to com.google.gson;
     opens ddc.server.model.transaction to com.google.gson;
@@ -31,11 +16,15 @@ module ddc.server {
     opens ddc.server.model.user to com.google.gson;
 
     opens ddc.server.network.response to com.google.gson;
+    opens ddc.server.network.request to com.google.gson;
+    opens ddc.server.network.message to com.google.gson;
+    opens ddc.server.network.client to com.google.gson;
+    opens ddc.server.network to com.google.gson;
 
     opens ddc.server.pattern.factory to com.google.gson, org.junit.platform.commons;
 
     opens ddc.server.exception to com.google.gson;
     opens ddc.server.pattern.Singleton to org.junit.platform.commons;
-    
+
     opens ddc.server.config to com.zaxxer.hikari;
 }
